1a4c5dfb37f7abafe1279d6c65207d07d1f00537
yookje <31889451+yookje@users.noreply.github.com>
2024-09-30 19:19:58 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
-#python3 train_and_inference_large.py --config ./configs/config1.yaml
+#tsp100
 python3 train_and_inference_large.py --config ./configs/config2.yaml
-#python3 train_and_inference_large.py --config ./configs/config7.yaml
-#python3 train_and_inference_large.py --config ./configs/config8.yaml
+#tsp1000
+python3 train_and_inference.py --config ./configs/config4.yaml
+#tsp500
 python3 train_and_inference.py --config ./configs/config6.yaml
-#python3 train_and_inference.py --config ./configs/config3.yaml
-#python3 train_and_inference.py --config ./configs/config4.yaml
-#python3 train_and_inference.py --config ./configs/config5.yaml
+#tsp50
+python3 train_and_inference_large.py --config ./configs/config8.yaml
